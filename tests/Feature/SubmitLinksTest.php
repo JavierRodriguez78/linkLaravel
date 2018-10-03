56b7e48a8f1c2e58b4bcf70788cf7d631af66bdf
Xavi <xavi@geekshubs.com>
2018-10-03 13:25:08 +0200

    First Test Integration

---
+++ tests/Feature/SubmitLinksTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SubmitLinksTest extends TestCase
+{
+   use RefreshDatabase;
+   /** @test  */
+   function guest_can_submit_a_new_link()
+   {
+       $response = $this->post('/submit', [
+           'title'=>'Example Title',
+           'url' =>'http://wwww.geekshubsacademy.com',
+           'description'=>'Example description'
+       ]);
+
+       $this->assertDatabaseHas('links',[
+           'title'=>'Example Title'
+       ]);
+
+       $response->assertStatus(302)
+       ->assertHeader('Location', url('/'));
+       $this ->get('/')->assertSee('Example Title');
+   }
+}
